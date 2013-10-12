326344355061729306e7668c2aacd51e4c291cec
ymmn <abdnimery92@gmail.com>
2013-10-12 02:19:16 -0500

    getting started

---
+++ src/hexapong.js
@@ -0,0 +1,7 @@
+//Draw a square on screen.
+var canvas = document.getElementById('canvas');
+var stage = new createjs.Stage(canvas);
+var shape = new createjs.Shape();
+shape.graphics.beginFill('rgba(255,0,0,1)').drawRoundRect(0, 0, 120, 120, 10);
+stage.addChild(shape);
+stage.update();
